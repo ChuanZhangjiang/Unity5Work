07006a51d180cd4e4e2789b9525d7a47091f0fb7
ChuanZhangjiang <ChuanZhangjiang@Gmail.com>
2016-04-27 23:19:20 +0800

    计算机图形学作业，完成第四次.

---
+++ Assets/Script/HelloUnity.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelloUnity : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//print ("Hello Unity 5 !");
+		Debug.Log("Hello Unity 5");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Script/PingPongMotion.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PingPongMotion : MonoBehaviour {
+
+    private Transform ThisTransform = null;
+    private Vector3 OrigPos = Vector3.zero;
+    public Vector3 MoveAxes = Vector3.zero;
+    public float Distance = 3.0f;
+
+    void Awake() {
+        ThisTransform = GetComponent<Transform>();
+        OrigPos = ThisTransform.position;
+    }
+
+	// Use this for initialization
+	void Start () {
+        ThisTransform.position = OrigPos + MoveAxes * Mathf.PingPong(Time.time, Distance);
+        OrigPos = ThisTransform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        ThisTransform.position = OrigPos + MoveAxes * Mathf.PingPong(Time.time, Distance);
+    }
+}
